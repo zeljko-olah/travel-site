684d0c57b40a340fb3edb349486a21fcaee99037
Zeljko <olah.zeljko@gmail.com>
2017-02-11 21:51:12 +0100

    Completed Lazy Loading of images

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11281,7 +11281,7 @@ var StickyHeader = function () {
   _createClass(StickyHeader, [{
     key: 'refreshWaypoints',
     value: function refreshWaypoints() {
-      this.lazyImages.load(function () {
+      this.lazyImages.on("load", function () {
         Waypoint.refreshAll();
       });
     }
